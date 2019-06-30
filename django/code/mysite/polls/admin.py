152a65bf696200f7bf9c44c36e055355bc7b2e92
wangtong <1249849263@qq.com>
2019-06-30 17:49:14 +0800

    配置django后台

--- django/code/mysite/polls/admin.py
+++ django/code/mysite/polls/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question,Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date','question_text']
+    search_fields = ['question_text','pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 
